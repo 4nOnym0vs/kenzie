588ec4a9da5b347f988105d39ca5cb9f01f2d232
Dabe Chua <devops_anon@protonmail.com>
2022-04-14 20:40:00 -0400

    Adding my name, my first java

---
+++ java-first-program/Main.java
@@ -0,0 +1,10 @@
+//The Hello World Program
+// Single line comment
+/* Multi
+line
+comments */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
